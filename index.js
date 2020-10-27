2337ea4ecd967e08e0b69de3d33f4af5d7e0a4cf
demetrio <demeuforia@gmail.com>
2020-10-27 09:50:21 +0100

    json el destructor

---
+++ index.js
@@ -0,0 +1,12 @@
+const mijson = [
+	{
+		pregunta: "En el mundo de Dragones y Mazmorras, ¿Quién es Elminster?",
+		respuestas: [
+			 "Un bardo",
+			 "Un mercader",
+             "Un mago",
+             "Un marinero"
+		],
+		respuestacorrecta: 'c'
+	},
+];
